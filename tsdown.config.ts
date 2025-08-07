f58d531c92482fc5c0da6cac6f4a5b51ed0e678f
Brett Zamir <brettz9@yahoo.com>
2025-08-07 09:55:58 +0800

    chore: attempt to fix tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,33 +2,56 @@ import {
   defineConfig,
 } from 'tsdown';
 
-export default defineConfig({
-  attw: {
-    level: 'error',
-    profile: 'node16',
-  },
-  dts: {
+export default defineConfig([
+  {
+    attw: {
+      level: 'error',
+      profile: 'node16',
+    },
+    dts: {
+      sourcemap: true,
+    },
+    entry: [
+      './src/index.js',
+    ],
+    format: [
+      'es',
+      'cjs',
+    ],
+    outputOptions: {
+      exports: 'default',
+    },
+    publint: {
+      level: 'error',
+      strict: true,
+    },
     sourcemap: true,
+    unbundle: true,
   },
-  entry: [
-    './src/index.js',
-    './src/getJsdocProcessorPlugin.js',
-    './src/iterateJsdoc.js',
-  ],
-  format: [
-    'es',
-    'cjs',
-  ],
-  outputOptions: {
-    exports: 'named',
+  {
+    attw: {
+      level: 'error',
+      profile: 'node16',
+    },
+    dts: {
+      sourcemap: true,
+    },
+    entry: [
+      './src/getJsdocProcessorPlugin.js',
+      './src/iterateJsdoc.js',
+    ],
+    format: [
+      'es',
+      'cjs',
+    ],
+    outputOptions: {
+      exports: 'named',
+    },
+    publint: {
+      level: 'error',
+      strict: true,
+    },
+    sourcemap: true,
+    unbundle: true,
   },
-  publint: {
-    level: 'error',
-    strict: true,
-  },
-  sourcemap: true,
-  unbundle: true,
-  unused: {
-    level: 'error',
-  },
-});
+]);
